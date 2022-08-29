5f89deb62a0866d23b28a6aff0a4fc82a2dc5385
Nina Palma <ninapalmadev@gmail.com>
2022-08-29 13:34:38 -0300

    cria exibição dos posts criados pelo usuário

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -15,27 +15,39 @@ const CreatePost = () => {
   const { user } = useAuthValue();
 
   const { insertDocument, response } = useInsertDocument("posts");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     //validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
 
     //criar array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos.");
+    }
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     //redirect to homepage
+    navigate("/");
   };
 
   return (
@@ -94,6 +106,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,46 @@
 //CSS
 import styles from "./Home.module.css";
 
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+//components
+import PostDetails from "../../components/PostDetails";
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
