26d395fde2ed528f8317bca88d2d6412cbb38ad7
Nina Palma <ninapalmadev@gmail.com>
2022-08-30 16:24:49 -0300

    Cria página de edição de post

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -59,8 +60,12 @@ function App() {
                 element={!user ? <Register /> : <Navigate to="/" />}
               />
               <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
+              <Route
                 path="/posts/create"
-                element={user ? <CreatePost /> : <Navigate to="/" />}
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
               />
               <Route
                 path="/dashboard"
